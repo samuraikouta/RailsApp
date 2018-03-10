ff8d3feda23e24dec2dde8e6758eb87a3fd90170
samuraikouta <kouta_work_sa@yahoo.co.jp>
2018-03-11 00:19:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,40 @@
+Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
+
+  get 'people/index'
+  get 'people', to: 'people#index'
+
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+
+  get 'people/delete/:id', to: 'people#delete'
+
+  get 'people/:id', to: 'people#show'
+
+  get 'dengonban/index'
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
+  get 'helo/index'
+  get 'helo', to: 'helo#index'
+  get 'helo/other'
+  post 'helo', to: 'helo#index'
+  post 'helo/index'
+end
