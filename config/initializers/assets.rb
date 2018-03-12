87ac0d1d455498ac91bc1d36e3051ff3676a7181
samuraikouta <kouta_work_sa@yahoo.co.jp>
2018-03-13 03:43:38 +0900

    アンケートサイトの追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -38,3 +38,6 @@ Rails.application.config.assets.precompile += %w( blogs.js )
 
 Rails.application.config.assets.precompile += %w( questions.css )
 Rails.application.config.assets.precompile += %w( questions.js )
+
+Rails.application.config.assets.precompile += %w( questionaries.css )
+Rails.application.config.assets.precompile += %w( questionaries.js )
