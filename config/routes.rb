b4d0cc25f735234f2f991c57d739d37c0849b45e
samuraikouta <kouta_work_sa@yahoo.co.jp>
2018-03-13 20:19:10 +0900

    kaminariの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :data_pages
   devise_for :accounts
   get 'ajax/index'
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312221909) do
+ActiveRecord::Schema.define(version: 20180313102641) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -67,6 +67,12 @@ ActiveRecord::Schema.define(version: 20180312221909) do
     t.integer "price"
     t.text "publisher"
     t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "data_pages", force: :cascade do |t|
+    t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
